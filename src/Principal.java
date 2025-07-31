7b979fdb7bd93018bf0a9be7fc9de570e759a1b9
AmandaCornelsen <cornelsen089@gmail.com>
2025-07-31 20:56:48 -0300

    Criação da classe Filme e inclusão de atributos e métodos

---
+++ src/Principal.java
@@ -0,0 +1,17 @@
+public class Principal {
+    public static void main(String[] args) {
+        Filme filme1 = new Filme();
+        filme1.nome = "O poderoso chefão";
+        filme1.anoDeLancamento = 1970;
+        filme1.duracaoEmMinutos = 180;
+        filme1.incluidoNoPlano = true;
+
+        filme1.exibeFichaTecnica();
+        filme1.avalia(8);
+        filme1.avalia(5);
+        filme1.avalia(10);
+        System.out.println(filme1.somaDasAvaliacoes);
+        System.out.println(filme1.totalDeAvaliacoes);
+        System.out.println(filme1.pegaMedia());
+    }
+}
