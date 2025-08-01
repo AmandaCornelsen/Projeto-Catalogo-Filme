8de4055a56edb9868e16db1cb259b5060ba5e1b1
AmandaCornelsen <cornelsen089@gmail.com>
2025-08-01 14:45:53 -0300

    Utilização de herança e criação de mais uma classe, serie

--- src/Principal.java
+++ src/Principal.java
@@ -1,17 +1,24 @@
+import br.com.screenmatch.modelos.Filme;
+import br.com.screenmatch.modelos.Serie;
+
 public class Principal {
     public static void main(String[] args) {
         Filme filme1 = new Filme();
-        filme1.nome = "O poderoso chefão";
-        filme1.anoDeLancamento = 1970;
-        filme1.duracaoEmMinutos = 180;
-        filme1.incluidoNoPlano = true;
+        filme1.setNome("O poderoso chefão");
+        filme1.setAnoDeLancamento(1970);
+        filme1.setDuracaoEmMinutos(180);
+        filme1.setIncluidoNoPlano(true);
 
         filme1.exibeFichaTecnica();
         filme1.avalia(8);
         filme1.avalia(5);
         filme1.avalia(10);
-        System.out.println(filme1.somaDasAvaliacoes);
-        System.out.println(filme1.totalDeAvaliacoes);
         System.out.println(filme1.pegaMedia());
+        System.out.println("Total de avaliações: " + filme1.getTotalDeAvaliacoes());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
     }
 }
